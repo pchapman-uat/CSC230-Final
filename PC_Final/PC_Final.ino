5cf4fdadcdc24a7725301c714cb521d13d0fdf8b
Preston Chapman <pchapman82070@uat.edu>
2024-04-18 14:55:05 -0700

    HTTP Connection
    Connection currently works and gets the response

--- PC_Final/PC_Final.ino
+++ PC_Final/PC_Final.ino
@@ -1,22 +1,24 @@
-#include <esp_crt_bundle.h>
-#include <ssl_client.h>
-#include <WiFiClientSecure.h>
-
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include "secrets.h"
 
 #include <M5StickCPlus.h>
 
-WiFiClientSecure client;
+HTTPClient client;
 
 void setup()
 {
+	M5.begin();
 	Serial.begin(115200);
 	WiFi.begin(SSID, PSWRD);
+	M5.Lcd.fillScreen(RED);
 	while (WiFi.status()!= WL_CONNECTED) {
         Serial.print(".");
         delay(500);
     }
+	M5.Lcd.fillScreen(GREEN);
 	Serial.println("Connected");
+	M5.Lcd.setRotation(3);
 
 	
 }
@@ -26,6 +28,17 @@ void loop()
 	M5.update();
 	if(M5.BtnA.wasReleased()){
 		client.begin(HTTP_POST);
-		int httpResponceCode = client.get();
+		int httpResponceCode = client.POST("");
+		if (httpResponceCode > 0) {
+			Serial.print("HTTP Response code: ");
+			Serial.println(httpResponceCode);
+			
+			String payload = client.getString();
+			Serial.println("Response payload:");
+			Serial.println(payload);
+		} else {
+			Serial.print("Error in HTTP GET request. HTTP Error code: ");
+			Serial.println(httpResponceCode);
+		}
 	}	
 }
