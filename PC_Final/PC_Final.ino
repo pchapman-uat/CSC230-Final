03dbe839a816293bf0c14f2d76f10fedf1af0025
Preston Chapman <pchapman82070@uat.edu>
2024-04-18 14:20:07 -0700

    Begin work on project

--- PC_Final/PC_Final.ino
+++ PC_Final/PC_Final.ino
@@ -1,9 +1,31 @@
+#include <esp_crt_bundle.h>
+#include <ssl_client.h>
+#include <WiFiClientSecure.h>
+
+#include "secrets.h"
+
+#include <M5StickCPlus.h>
+
+WiFiClientSecure client;
+
 void setup()
 {
+	Serial.begin(115200);
+	WiFi.begin(SSID, PSWRD);
+	while (WiFi.status()!= WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+	Serial.println("Connected");
+
 	
 }
 
 void loop()
 {
-	
+	M5.update();
+	if(M5.BtnA.wasReleased()){
+		client.begin(HTTP_POST);
+		int httpResponceCode = client.get();
+	}	
 }
