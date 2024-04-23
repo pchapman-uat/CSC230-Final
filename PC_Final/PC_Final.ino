e932f65adc184c2719098d2df1e722a280cf9894
Preston Chapman <pchapman82070@uat.edu>
2024-04-23 16:03:20 -0700

    Added post and changed get

--- PC_Final/PC_Final.ino
+++ PC_Final/PC_Final.ino
@@ -8,6 +8,10 @@
 
 HTTPClient client;
 DynamicJsonBuffer jsonBuffer;
+
+int id;
+
+int mode = 0;
 void setup()
 {
 	M5.begin();
@@ -29,22 +33,43 @@ void loop()
 {
 	M5.update();
 	if(M5.BtnA.wasReleased()){
-		client.begin(HTTP_POST);
-		int httpResponceCode = client.POST("");
-		if (httpResponceCode > 0) {
-			Serial.print("HTTP Response code: ");
-			Serial.println(httpResponceCode);
-			
-			String payload = client.getString();
-			Serial.println("Response payload:");
-			Serial.println(payload);
+		if(mode == 0){
+			M5.Lcd.println("Mode: GET");
+			client.begin(HTTP_GET);
+			int httpResponceCode = client.POST("");
+			if (httpResponceCode > 0) {
+				Serial.print("HTTP Response code: ");
+				Serial.println(httpResponceCode);
+				
+				String payload = client.getString();
+				Serial.println("Response payload:");
+				Serial.println(payload);
 
-			JsonObject& response = jsonBuffer.parseObject(payload);
-			response.printTo(Serial);
-			M5.Lcd.println(response["Start"].asString());
-		} else {
-			Serial.print("Error in HTTP GET request. HTTP Error code: ");
-			Serial.println(httpResponceCode);
+				JsonObject& response = jsonBuffer.parseObject(payload);
+				response.printTo(Serial);
+				M5.Lcd.println(response["Start"].asString());
+				id = response["ID"].as<int>();
+				M5.Lcd.println(response["ID"].asString());
+			} else {
+				Serial.print("Error in HTTP GET request. HTTP Error code: ");
+				Serial.println(httpResponceCode);
+			}
+		} else if (mode == 1){
+			M5.Lcd.println("Mode: POST");
+			client.begin(HTTP_POST);
+			client.addHeader("Content-Type", "application/json");
+			int httpResponceCode = client.POST("{\"id\": " + String(id) + ", \"end\": \"" + "2024-04-23T22:02:23Z" + "\", \"people\": " + String(2) + "}");
+			if (httpResponceCode > 0) {
+				Serial.print("HTTP Response code: ");
+				Serial.println(httpResponceCode);
+				String payload = client.getString();
+				Serial.println("Response payload:");
+				Serial.println(payload);
+			} else {
+				Serial.print("Error in HTTP GET request. HTTP Error code: ");
+				Serial.println(httpResponceCode);
+			}
 		}
 	}	
+	else if(M5.BtnB.wasReleased()) mode = (mode + 1) % 2;
 }
